b725ac7ba657c1257848a42890fc0fb74d756c7f
omi015 <fuffy_813@hotmail.com>
2024-11-29 22:48:17 -0500

    se corriguio login y funciones

--- src/app/controllers/Admin.mjs
+++ src/app/controllers/Admin.mjs
@@ -1,15 +1,14 @@
 import { Router } from "express";
+import { Products } from "../modules/Products.mjs";
 
 export const RouterPage = Router();
 
-
-RouterPage.route("/login").get( (req, res) => {
-    res.render("page/login");
-
+RouterPage.route("/login").get((req, res) => {
+  res.render("page/login", { login: Boolean(req.session.user) });
+});
+RouterPage.route("/logout").get((req, res) => {
+  req.session.destroy(function (err) {
+    console.log("destroy");
+    res.redirect("/");
   });
-  RouterPage.route("/logout").get((req, res) =>{
-    req.session.destroy(function(err) {
-        console.log("destroy")
-        res.redirect("/")
-      })
-  })
+});
